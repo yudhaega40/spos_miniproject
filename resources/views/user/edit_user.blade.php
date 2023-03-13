b577e29cc77a6ebd38136fca508afa084d7a89c4
yudhaega40 <66153500+yudhaega40@users.noreply.github.com>
2023-03-13 16:09:57 +0700

    menyamakan layout edit/new user dan ubah password

--- resources/views/user/edit_user.blade.php
+++ resources/views/user/edit_user.blade.php
@@ -1,47 +1,58 @@
-<x-guest-layout>
-    <h1 class="font-semibold text-xl text-gray-800 dark:text-gray-200 mb-4 text-center">
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
         {{ __('Edit User') }}
-    </h1>
-    <form method="POST" action="{{ route('simpan_edit') }}">
-        @csrf
+        </h2>
+    </x-slot>
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" value="{{ $user->name }}" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+    <div class="py-12">
+        <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <form method="POST" action="{{ route('simpan_edit') }}">
+                        @csrf
+
+                        <!-- Name -->
+                        <div>
+                            <x-input-label for="name" :value="__('Name')" />
+                            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" value="{{ $user->name }}" required autofocus autocomplete="name" />
+                            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                        </div>
+
+                        <!-- Email Address -->
+                        <div class="mt-4">
+                            <x-input-label for="email" :value="__('Email')" />
+                            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" value="{{ $user->email }}" required autocomplete="username" />
+                            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                        </div>
+
+                        <!-- Role -->
+                        <div class="mt-4">
+                            <x-input-label for="role" :value="__('Role')" />
+                            <select id="role" class="block mt-1 w-full p-2.5 border border-gray-300 text-gray-900 text-sm rounded-lg" name="role" value="{{ $user->role }}" required autocomplete="username" />
+                                <option value = "1">Author</option>
+                                <option value = "2">Editor</option>
+                                <option value = "3">Admin</option>
+                            </select>
+                            <x-input-error :messages="$errors->get('role')" class="mt-2" />
+                        </div>
+
+                        <input type="hidden" id="id_user" name="id_user" value="{{ $user->id }}">
+
+                        <div class="flex items-center justify-end mt-4">
+                            <a href="/user" type="submit" class="show_confirm btn bg-red-700 hover:bg-red-500 text-white py-1 px-2 rounded" data-toggle="tooltip" title='Delete'>
+                                Cancel
+                            </a>
+                            <a href="/ubah_password/{{ $user->id }}" class="ml-4 bg-green-700 hover:bg-green-500 text-white py-1 px-2 rounded">
+                                Change Password 
+                            </a>
+                            <x-primary-button class="ml-4">
+                                {{ __('Update') }}
+                            </x-primary-button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" value="{{ $user->email }}" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Role -->
-        <div class="mt-4">
-            <x-input-label for="role" :value="__('Role')" />
-            <select id="role" class="block mt-1 w-full" name="role" value="{{ $user->role }}" required autocomplete="username" />
-                <option value = "1">Author</option>
-                <option value = "2">Editor</option>
-                <option value = "3">Admin</option>
-            </select>
-            <x-input-error :messages="$errors->get('role')" class="mt-2" />
-        </div>
-
-        <input type="hidden" id="id_user" name="id_user" value="{{ $user->id }}">
-
-        <div class="flex items-center justify-end mt-4">
-            <a href="/user" type="submit" class="show_confirm btn bg-red-700 hover:bg-red-500 text-white py-1 px-2 rounded" data-toggle="tooltip" title='Delete'>
-                Cancel
-            </a>
-            <a href="/ubah_password/{{ $user->id }}" class="ml-4 bg-green-700 hover:bg-green-500 text-white py-1 px-2 rounded">
-                Change Password 
-            </a>
-            <x-primary-button class="ml-4">
-                {{ __('Update') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </div>
+</x-app-layout>
--- resources/views/user/ubah_password.blade.php
+++ resources/views/user/ubah_password.blade.php
@@ -1,32 +1,43 @@
-<x-guest-layout>
-    <h1 class="font-semibold text-xl text-gray-800 dark:text-gray-200 mb-1 text-center">
-        {{ __('Change Password') }}
-    </h1>
-    <p class="mb-4 text-center">User: {{$user->name}}</p>
-    <form method="POST" action="{{ route('simpan_ubah_password') }}">
-        @csrf
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        {{ __('Ubah Password') }}
+        </h2>
+    </x-slot>
 
-        <div>
-            <x-input-label for="password" :value="__('Password Baru')" />
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autofocus/>
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+    <div class="py-12">
+        <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <p class="mb-4 text-center text-md font-semibold">User: {{$user->name}}</p>
+                    <form method="POST" action="{{ route('simpan_ubah_password') }}">
+                        @csrf
+
+                        <div>
+                            <x-input-label for="password" :value="__('Password Baru')" />
+                            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autofocus/>
+                            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                        </div>
+
+                        <div class="mt-4">
+                            <x-input-label for="password_confirmation" :value="__('Konfirmasi Password')" />
+                            <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autofocus/>
+                            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                        </div>
+
+                        <input type="hidden" id="id_user" name="id_user" value="{{ $user->id }}">
+
+                        <div class="flex items-center justify-end mt-4">
+                            <a href="/edit_user/{{ $user->id }}" type="submit" class="show_confirm btn bg-red-700 hover:bg-red-500 text-white py-1 px-2 rounded" data-toggle="tooltip" title='Delete'>
+                                Cancel
+                            </a>
+                            <x-primary-button class="ml-4">
+                                {{ __('Simpan') }}
+                            </x-primary-button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Konfirmasi Password')" />
-            <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autofocus/>
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <input type="hidden" id="id_user" name="id_user" value="{{ $user->id }}">
-
-        <div class="flex items-center justify-end mt-4">
-            <a href="/edit_user/{{ $user->id }}" type="submit" class="show_confirm btn bg-red-700 hover:bg-red-500 text-white py-1 px-2 rounded" data-toggle="tooltip" title='Delete'>
-                Cancel
-            </a>
-            <x-primary-button class="ml-4">
-                {{ __('Simpan') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </div>
+</x-app-layout>
